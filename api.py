a1762b2fdc49a60c68c4c56181761da803f6ad17
Kenzy Young <zxc114@mocs.utc.edu>
2021-03-26 03:58:56 -0400

    Add enroll/get_user_templates to api.py

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import codecs
 import configuration
 
 config = configuration.load()
+valid_classifications = ['l', 'r', 'a', 't', 'w', 's']
 
 
 def request(api, args):
@@ -23,3 +24,23 @@ def request(api, args):
     result = json.load(reader(response))
     response.release_conn()
     return result
+
+
+def check_username_length(username):
+    return (len(username) >= 4) and (len(username) <= 32)
+
+
+def enroll(username, classification, template):
+    if not check_username_length(username):
+        return False
+    if classification not in valid_classifications:
+        return False
+
+    return request('enroll', {'username': username, 'classification': classification, 'template': template})
+
+
+def get_user_templates(username):
+    if not check_username_length(username):
+        return False
+
+    return request('get_user_templates', {'username': username})
